443933b816a38244fb757640690fadab70842303
minsoo Kim <minsoozzz@safety-as.com>
2021-02-24 15:03:17 +0900

    AgencyController.java edit
    대행 서비스 완료

---
+++ module-common/src/main/java/com/safetyas/sds/common/model/TranslationAgencyProgressDTO.java
@@ -0,0 +1,31 @@
+package com.safetyas.sds.common.model;
+
+import com.querydsl.core.annotations.QueryProjection;
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class TranslationAgencyProgressDTO extends CommonDTO implements Serializable {
+
+  private static final long serialVersionUID = -370168335705926088L;
+
+  private Long translationAgencySeq;
+  private String state;
+  private String note;
+
+  @QueryProjection
+  public TranslationAgencyProgressDTO(String state, String note, LocalDateTime inDate) {
+    super(inDate);
+    this.state = state;
+    this.note = note;
+  }
+}
